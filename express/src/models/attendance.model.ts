6324855f95a07c02a1bfd5a6b22a87e992beea8c
s-m-quadri <88645248+s-m-quadri@users.noreply.github.com>
2025-04-07 11:50:18 +0530

    Update attendance scheme

--- express/src/models/attendance.model.ts
+++ express/src/models/attendance.model.ts
@@ -3,7 +3,10 @@ import mongoose from "mongoose";
 export default mongoose.model(
   "attendance",
   new mongoose.Schema({
-    date: { type: Number, default: Date.now() },
+    date: { type: String, required: true, trim: true },
+    isOut: { type: Boolean, default: false },
+    timeIn: { type: Number, default: Date.now() },
+    timeOut: { type: Number, default: Date.now() + 28800000 },
     reader: { type: mongoose.Schema.Types.ObjectId, ref: "reader", required: true },
     students: [{ type: mongoose.Schema.Types.ObjectId, ref: "user", required: true }],
     reason: { type: String, enum: ["lecture", "lab", "exam", "seminar", "workshop", "extracurricular", "general"], default: "general" },
