29aba5d0afac59bdfd9c64c59ced4ceb12dc425e
s-m-quadri <88645248+s-m-quadri@users.noreply.github.com>
2025-04-14 23:29:18 +0530

    Add RFID and related providers

--- flutter/lib/main.dart
+++ flutter/lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:taptag/model/attendance.model.dart';
+import 'package:taptag/model/reader.model.dart';
 import 'package:taptag/screen/auth_screen.dart';
 import 'package:taptag/model/theme.model.dart';
 import 'package:taptag/model/user.model.dart';
+import 'package:taptag/screen/rfid_screen.dart';
 
 void main() {
   runApp(
@@ -10,6 +13,9 @@ void main() {
       providers: [
         ChangeNotifierProvider(create: (_) => UserProvider()),
         ChangeNotifierProvider(create: (_) => ThemeProvider()),
+        ChangeNotifierProvider(create: (_) => ReaderProvider()),
+        ChangeNotifierProvider(create: (_) => RFIDService()),
+        ChangeNotifierProvider(create: (_) => AttendanceProvider()),
       ],
       child: const MyApp(),
     ),
